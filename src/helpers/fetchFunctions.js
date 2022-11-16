21cbed5b51bc88aa616c93b6289a4dfb905e5662
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-16 15:53:23 -0300

    Requisito 2 - Implementada a chave result

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,10 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = (productType) => {
+  if (!productType) throw new Error('Termo de busca não informado');
+  const result = async () => {
+    await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${productType}`);
+  }
+  return result;
 };
