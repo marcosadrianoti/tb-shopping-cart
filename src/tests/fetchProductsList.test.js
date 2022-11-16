ddc2b09d4ddc06a7db11d191ee5620bdbd25bea6
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-16 15:30:04 -0300

    Testes do requisito 1 implementados

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -8,14 +8,24 @@ describe('Teste a função fetchProductsList', () => {
     expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async() => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
   });
 
+  it('o retorno da função fetchProductsList com o argumento \'computador\' é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const response = await fetchProductsList('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+  it('Chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: \'Termo de busca não informado\'', () => {
+    expect(async() => { await fetchProductsList() }).toThrow('Termo de busca não informado');
+  });
   // it('...', () => {
   // });
 });
