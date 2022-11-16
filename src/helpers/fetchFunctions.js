6214476c88103ab12335761fc27d1ffb0cb35de9
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-16 17:50:24 -0300

    Requisito 2 - corrigindo erro

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,8 +2,9 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = async(productType) => {
-    const arrayDatas = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${productType}`);
-    const data = await arrayDatas.json();
-    return data.results;
+export const fetchProductsList = async (productType) => {
+  if (!productType) throw new Error('Termo de busca não informado');
+  const arrayDatas = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${productType}`);
+  const data = await arrayDatas.json();
+  return data.results;
 };
