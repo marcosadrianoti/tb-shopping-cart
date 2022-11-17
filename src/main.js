529ddffbb858e438d0fd72a52ccd9b07527afa36
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-17 18:19:31 -0300

    Requisito 9

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { searchCep } from './helpers/cepFunctions';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { getSavedCartIDs } from './helpers/cartFunctions';
+import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
+import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
 import { addMsgElem, removeMsgElem } from './helpers/msgsFunctions';
 import './style.css';
 
@@ -15,6 +16,14 @@ try {
     document.querySelector('.products').appendChild(productElement);
   });
   removeMsgElem();
+
+  const savedCartIDs = (getSavedCartIDs());
+  const savedCartPromises = savedCartIDs.map(async (id) => fetchProduct(id));
+  Promise.all(savedCartPromises)
+    .then((arrayProducts) => arrayProducts.forEach((product) => {
+      document.querySelector('.cart__products')
+        .appendChild(createCartProductElement({ ...product }));
+    }));
 } catch (error) {
   removeMsgElem();
   addMsgElem('Algum erro ocorreu, recarregue a página e tente novamente');
