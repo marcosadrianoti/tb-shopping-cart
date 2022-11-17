90135fe418d23e5ab6fa8e55cc2940097290790e
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-17 15:24:42 -0300

    Requisito 6

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -1,5 +1,5 @@
 import './mocks/fetchSimulator';
-import { fetchProductsList } from '../helpers/fetchFunctions';
+import { fetchProductsList, fetchProduct } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
@@ -19,13 +19,36 @@ describe('Teste a função fetchProductsList', () => {
     expect(fetch).toHaveBeenCalledWith(endpoint);
   });
 
-  it('o retorno da função fetchProductsList com o argumento \'computador\' é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+  it('O retorno da função fetchProductsList com o argumento \'computador\' é uma estrutura de dados igual ao objeto computadorSearch', async () => {
     await expect(fetchProductsList('computador')).resolves.toEqual(computadorSearch);
   });
 
   it('Chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: \'Termo de busca não informado\'', () => {
     expect(fetchProductsList()).rejects.toEqual(new Error('Termo de busca não informado'));
   });
-  // it('...', () => {
-  // });
+});
+
+describe('Teste a função fetchProduct', () => {
+  it('fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
+  });
+
+  it('fetch é chamado ao executar fetchProduct', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('fetch é chamado com o endpoint correto ao executar fetchProduct', async () => {
+    await fetchProduct('MLB1405519561');
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1405519561';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it('O retorno da função fetchProduct com o argumento \'MLB1405519561\' é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    await expect(fetchProduct('MLB1405519561')).resolves.toEqual(produto);
+  });
+
+  it('Chamar a função fetchProduct sem argumento, retorna um erro com a mensagem: \'ID não informado\'', () => {
+    expect(fetchProduct()).rejects.toEqual(new Error('ID não informado'));
+  });
 });
