c800a814f2c3679560d5e15bac5b9ef0e11b670b
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-17 12:13:29 -0300

    Requisito 4

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -4,7 +4,9 @@ export const fetchProduct = () => {
 
 export const fetchProductsList = async (productType) => {
   if (!productType) throw new Error('Termo de busca não informado');
+  // addLoadingElem();
   const arrayDatas = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${productType}`);
   const data = await arrayDatas.json();
+  // removeLoadingElem();
   return data.results;
 };
--- src/main.js
+++ src/main.js
@@ -5,9 +5,24 @@ import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+export const addLoadingElem = () => {
+  const loadingElem = document.createElement('span');
+  loadingElem.classList.add('loading');
+  loadingElem.innerHTML = '<strong>carregando...</strong>';
+  document.querySelector('.products').appendChild(loadingElem);
+};
+
+export const removeLoadingElem = () => {
+  const loadingElem = document.querySelector('.loading');
+  loadingElem.remove();
+};
+
+addLoadingElem();
 const productsList = await fetchProductsList('computador');
 
 productsList.forEach((product) => {
   const productElement = createProductElement({ ...product });
   document.querySelector('.products').appendChild(productElement);
 });
+removeLoadingElem();
+
